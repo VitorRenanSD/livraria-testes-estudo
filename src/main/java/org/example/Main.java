eb12c4f80cde5ee0ce4896184b4c8d081ae1259b
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-02-19 11:09:29 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Conexao banco1 = new Conexao("localhost", "5432", "livraria_teste", "postgres", "@skydiver2442!");
 
-
+        banco1.conectaBanco();
     }
 }
