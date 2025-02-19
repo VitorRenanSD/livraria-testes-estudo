eaf1e2c2880f6551712009c203752902fb4f2dee
Vitor <167242612+VitorRenanSD@users.noreply.github.com>
2025-02-19 11:09:40 -0300

    Update Conexao.java

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -2,31 +2,39 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Conexao {
-    public static void main(String[] args) throws SQLException {
+    private final String ip;
+    private final String porta;
+    private final String banco;
+    private final String usuario;
+    private final String senha;
+
+    public Conexao(String ip, String porta, String banco, String usuario, String senha) {
+        this.ip = ip;
+        this.porta = porta;
+        this.banco = banco;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public void conectaBanco() throws SQLException {
         Connection conexao = null;
 
         try {
             Class.forName("org.postgresql.Driver");
-            conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/livraria_teste", "postgres", "@skydiver2442!");
-            ResultSet rsCliente = conexao.createStatement().executeQuery("SELECT * FROM cliente");
-
-            while (rsCliente.next()) {
-                System.out.println("Nome: " + rsCliente.getString("nome"));
-            }
+            conexao = DriverManager.getConnection("jdbc:postgresql://" + this.ip + ":" + this.porta + "/" + this.banco, this.usuario, this.senha);
+            System.out.println("Banco conectado com sucesso!");
 
         } catch (ClassNotFoundException ex) {
             System.out.println("Driver do banco de dados nao encontrado");
         } catch (SQLException ex) {
-            System.out.println("Erro ao acessar o banco: " + ex.getMessage());;
+            System.out.println(ex.getMessage());
         } finally {
             if (conexao != null) {
                 conexao.close();
             }
-
         }
     }
 }
